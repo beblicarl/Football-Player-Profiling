6705702cf46ebaef116ac90d1e47d76f58b59632
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-01 15:20:39 +0000

    ensured that the routes are authenticated

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,26 +5,14 @@ function auth() {
   const secret = process.env.SECRET;
   return expressJwt({
     secret,
-    algorithms: ["HS256"],
-    isRevoked: isRevoked,
+    algorithms: ["HS256"]
+
   }).unless({
     path: [
-      { url: /\/api\/v1\/profile(.*)/, methods: ["POST", "OPTIONS"] },
-      { url: /\/api\/v1\/profile(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/profile(.*)/, methods: ["PATCH", "OPTIONS"] },
-      { url: /\/api\/v1\/profile(.*)/, methods: ["DELETE", "OPTIONS"] },
       `${api}user/login`,
-      `${api}user/logout`,
       `${api}user/register`,
-      `${api}profile`
     ],
   });
-}
-async function isRevoked(req, payload, done) {
-  if (!payload.isAdmin) {
-    done(null, true);
-  }
-  done();
 }
 
 module.exports = auth;

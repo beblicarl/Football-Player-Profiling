39243d72f22f0d7b35e2944e5adef15d4783a52f
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-02 14:52:17 +0000

    imported the schema validation and ensured some routes were validated

--- router/routes.js
+++ router/routes.js
@@ -1,31 +1,47 @@
 const express = require("express");
 const router = express.Router();
+
 //profile controller
 const {
   addProfile,
   getSingleProfile,
-  getProfile,
+  getAllProfiles,
   deleteProfile,
   updateProfile
 } = require("../controllers/Profile");
 
-const { registerUser, getAllUser, loginUser,logoutUser } = require("../controllers/User");
+const { 
+      registerUser, 
+      getAllUser, 
+      loginUser,
+      logoutUser } = require("../controllers/User");
+
+const {
+    registerUserSchema,
+    loginSchema,
+    addProfileSchema,
+    getProfileSchema,
+    updateProfileSchema,
+    deleteProfileSchema
+} = require("../schema")
+
+const validateSchema = require("../middleware/validateSchema")
 
 //profile route
 router.route("/profile")
-      .post(addProfile)
-      .get(getProfile)
-      .delete(deleteProfile)
-
+      .post( validateSchema(addProfileSchema), addProfile)
+      .get(getAllProfiles)
+      
 router.route("/profile/:id")
-      .get(getSingleProfile)
-      .patch(updateProfile);
+      .get( validateSchema(getProfileSchema), getSingleProfile)
+      .patch( validateSchema(updateProfileSchema) , updateProfile)
+      .delete(validateSchema(deleteProfileSchema) , deleteProfile)
 
 //user route
 router.route("/user/register")
-      .post(registerUser);
+      .post( validateSchema(registerUserSchema), registerUser);
 router.route("/user/login")
-      .post(loginUser);
+      .post(validateSchema(loginSchema), loginUser);
 router.route("/user/logout")
       .get(logoutUser);
 router.route("/user")
